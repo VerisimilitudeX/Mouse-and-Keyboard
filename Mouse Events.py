acf67aa3ddd8e133eca7de66b3ae001808564b6d
Verisimilitude <acharyapiyush1@outlook.com>
2022-03-01 12:58:01 -0800

    Create Mouse Events.py

---
+++ Mouse Events.py
@@ -0,0 +1,31 @@
+# Libraries
+import random
+import pygame
+pygame.init()
+
+# Setup
+r = random.randint(0, 255)
+g = random.randint(0, 255)
+b = random.randint(0, 255)
+
+# Window
+window = pygame.display.set_mode([200, 200])
+window.fill((r, g, b))
+pygame.display.flip()
+
+# Main loop
+running = True
+while running:
+
+    # Event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            print("Mouse down ")
+            pos = pygame.mouse.get_pos()
+            print(pos)
+        elif event.type == pygame.MOUSEBUTTONUP:
+            print("Mouse up" )
+            pos = pygame.mouse.get_pos()
+            print(pos)
